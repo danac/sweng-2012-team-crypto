9b5ae3f9e16c0999364fbed7b69e79398778aadf
Dana Christen <dana.christen@epfl.ch>
2012-09-28 18:32:17 +0200

    Added class Question

---
+++ Sweng2012QuizApp/src/epfl/sweng/showquestions/Question.java
@@ -0,0 +1,29 @@
+public class Question {
+    private String question;
+    private String[] answers;
+    private int rightAnswer;
+    private String[] tags;
+    private String owner;
+    private int id;
+    
+    public String getQuestion(){
+    	return question;
+    }
+    
+    public String[] getAnswers(){
+    	return answers;
+    }
+      
+    
+    public Question(){
+        super();
+        question = "Choose an answer:";
+    	answers = new String[] {
+    	        "Answer 1",
+    	        "Answer 2",
+    	        "Answer 3",
+    	        "Answer 4"
+    	    };
+    }
+
+}
