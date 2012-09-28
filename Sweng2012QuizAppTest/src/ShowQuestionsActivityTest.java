719ecc6a2fc6de109c0e01a8f6034ecd8af9e53a
Dana Christen <dana.christen@epfl.ch>
2012-09-28 15:37:25 +0200

    Basic track > Unit tests (2/2)

---
+++ Sweng2012QuizAppTest/src/ShowQuestionsActivityTest.java
@@ -0,0 +1,17 @@
+import epfl.sweng.showquestions.ShowQuestionsActivity;
+import android.test.ActivityInstrumentationTestCase2;
+
+/**
+ * First test case...
+ */
+public class ShowQuestionsActivityTest extends
+		ActivityInstrumentationTestCase2<ShowQuestionsActivity> {
+
+	public ShowQuestionsActivityTest() {
+		super(ShowQuestionsActivity.class);
+	}
+	
+	public void testHello() {
+		fail("Not yet implemented");
+	}
+}
