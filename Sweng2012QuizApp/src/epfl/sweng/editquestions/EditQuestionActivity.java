2305fd352635642e6d0e9546ab325e47dbce1886
Dana Christen <dana.christen@epfl.ch>
2012-10-12 16:35:26 +0200

    Create new blank activities EditQuestionActivity and MainActivity, update Manifest

---
+++ Sweng2012QuizApp/src/epfl/sweng/editquestions/EditQuestionActivity.java
@@ -0,0 +1,23 @@
+package epfl.sweng.editquestions;
+
+import epfl.sweng.R;
+import epfl.sweng.R.layout;
+import epfl.sweng.R.menu;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class EditQuestionActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_question);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_edit_question, menu);
+        return true;
+    }
+}
