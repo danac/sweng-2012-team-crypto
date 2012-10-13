abc857711b60bd3bb4a0833e52410e7921c8bf6e
Emmanuel Froustey <emmanuel.froustey@epfl.ch>
2012-10-13 17:14:11 +0200

    Implement the 'add answer' function

--- Sweng2012QuizApp/src/epfl/sweng/editquestions/EditQuestionActivity.java
+++ Sweng2012QuizApp/src/epfl/sweng/editquestions/EditQuestionActivity.java
@@ -1,12 +1,13 @@
 package epfl.sweng.editquestions;
 
-import epfl.sweng.R;
-import epfl.sweng.R.layout;
-import epfl.sweng.R.menu;
-import epfl.sweng.quizquestions.QuizQuestion;
-import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import epfl.sweng.R;
+import epfl.sweng.quizquestions.QuizQuestion;
 
 /**
  * Activity enabling the user to edit a question
@@ -18,12 +19,29 @@ public class EditQuestionActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_question);
+        manageAnswers();
     }
 
     public QuizQuestion createQuestionFromUser() {
     	QuizQuestion question = new QuizQuestion();
-    	// TODO Implement a listener that will hydrate the question
-    	// and check that it is valid using question.auditErrors()
+    	// TODO Implement a listener that will hydrate the question and check its validity
+    	return question;
+    }
+    
+    public void manageAnswers() {
+    	final LinearLayout answersContainer = (LinearLayout) findViewById(R.id.edit_answers_container);
+    	
+    	// TODO Remove questions, mark them as correct or not
+    	
+    	// Add new answers on clicking '+' button
+    	final Button newQuestionButton = (Button) findViewById(R.id.edit_button_new_answer);
+    	newQuestionButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				View newAnswer = getLayoutInflater().inflate(R.layout.edit_new_answer, null);
+				answersContainer.addView(newAnswer, answersContainer.getChildCount()-1);
+			}
+		});
     }
     
     
