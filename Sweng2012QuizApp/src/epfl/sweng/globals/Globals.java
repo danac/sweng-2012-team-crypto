614a36a910921e0468fb509d4e581bf7bd469218
Cyril Misev <cyril.misev@epfl.ch>
2012-10-16 16:58:41 +0200

    make Globals final

--- Sweng2012QuizApp/src/epfl/sweng/globals/Globals.java
+++ Sweng2012QuizApp/src/epfl/sweng/globals/Globals.java
@@ -3,9 +3,10 @@ package epfl.sweng.globals;
 /**
  * Static global variables
  */
-public class Globals {
+public final class Globals {
 
 	public final static String RANDOM_QUESITON_URL = "https://sweng-quiz.appspot.com/quizquestions/random";
 	public final static String SUBMIT_QUESITON_URL = "https://sweng-quiz.appspot.com/quizquestions/";
 	public final static int MAX_NUMBER_OF_ANSWERS = 10;
+		
 }
