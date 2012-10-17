cac4bc653b8201755a13b1993dcafa63b068e258
Cyril Misev <cyril.misev@epfl.ch>
2012-10-17 09:45:30 +0200

    Add tests

--- Sweng2012QuizAppTest/src/epfl/sweng/test/EditQuestionActivityTest.java
+++ Sweng2012QuizAppTest/src/epfl/sweng/test/EditQuestionActivityTest.java
@@ -2,7 +2,6 @@ package epfl.sweng.test;
 import org.json.JSONException;
 
 import android.test.ActivityInstrumentationTestCase2;
-import android.view.View;
 import android.widget.EditText;
 
 import com.jayway.android.robotium.solo.Solo;
@@ -18,7 +17,11 @@ public class EditQuestionActivityTest extends
 	
 	private Solo solo;
 	private final static int WAIT_TIME = 4000;
-	private final static String TEST_QUESTION = "Test question..."; 
+	private final static String TEST_QUESTION = "Test question...";
+	private final static String TEST_FALSEANSWER = "False Answer";
+	private final static String TEST_RIGHTANSWER = "Right Answer";
+	private final static String TEST_TAGS = "test";
+	
 	
 	public EditQuestionActivityTest() {
 		super(EditQuestionActivity.class);
@@ -45,15 +48,29 @@ public class EditQuestionActivityTest extends
 		assertTrue(solo.searchText("\u2714"));
 		solo.clickOnButton("\u2714");
     	assertTrue(solo.waitForText("One answer should be marked as correct"));		
-		
-		for (View v: solo.getViews()) {
-			if (v.getId() == epfl.sweng.R.id.edit_question_text) {
-				solo.enterText((EditText) v, TEST_QUESTION);
+    	solo.clickOnButton("\\+");
+    	boolean rightAnswerEntered = false;
+		for (EditText et: solo.getCurrentEditTexts()) {
+			if (et.getTag().toString() 
+				== getActivity().getResources().getText(epfl.sweng.R.string.edit_question_hint)) {
+				solo.enterText(et, TEST_QUESTION);
+			} else if (et.getTag().toString() 
+				== getActivity().getResources().getText(epfl.sweng.R.string.edit_tags_hint)) {
+				solo.enterText(et, TEST_TAGS);
+			} else if (et.getTag().toString() 
+				== getActivity().getResources().getText(epfl.sweng.R.string.edit_answer_hint)) {
+				
+				if (rightAnswerEntered) {
+					solo.enterText(et, TEST_FALSEANSWER);
+				} else {
+					solo.enterText(et, TEST_RIGHTANSWER);
+					rightAnswerEntered = true;
+				}
 			}
 		}
-		 
-		 //solo.clickOnButton("Submit");
-
+		solo.clickOnButton("\u2718");
+		solo.clickOnButton("Submit");
+		assertTrue(solo.waitForText("\u2714 Question successfully submitted"));
 	}
 
 	
