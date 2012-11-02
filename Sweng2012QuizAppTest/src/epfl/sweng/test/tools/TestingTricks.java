6315353839d93853064ce62767ded230a3b15bf5
Cyril Misev <cyril.misev@epfl.ch>
2012-11-02 15:31:37 +0100

    Test if Authentication neccessary

--- Sweng2012QuizAppTest/src/epfl/sweng/test/tools/TestingTricks.java
+++ Sweng2012QuizAppTest/src/epfl/sweng/test/tools/TestingTricks.java
@@ -12,17 +12,21 @@ public class TestingTricks {
 	private final static String TEST_USERNAME = "valid";
 	private final static String TEST_PASSWORD = "tutu";
 	
-	public static void authenticateMe(Activity activity, Solo solo) {
-		
-		for (EditText et: solo.getCurrentEditTexts()) {
-			if (et.getTag().toString() 
-				== activity.getResources().getText(epfl.sweng.R.string.auth_login_hint)) {
-				solo.enterText(et, TEST_USERNAME);
-			} else if (et.getTag().toString()
-				== activity.getResources().getText(epfl.sweng.R.string.auth_pass_hint)) {
-				solo.enterText(et, TEST_PASSWORD);
-			}
-		}				
-		solo.clickOnButton("Log in using Tequila");
+	public static void authenticateMe(Solo solo) {
+		Activity activity = solo.getCurrentActivity();
+		System.out.println(activity.getClass().getName());
+		if (activity.getClass().getName().equals("AuthenticationActivity")) {
+			
+			for (EditText et: solo.getCurrentEditTexts()) {
+				if (et.getTag().toString() 
+					== activity.getResources().getText(epfl.sweng.R.string.auth_login_hint)) {
+					solo.enterText(et, TEST_USERNAME);
+				} else if (et.getTag().toString()
+					== activity.getResources().getText(epfl.sweng.R.string.auth_pass_hint)) {
+					solo.enterText(et, TEST_PASSWORD);
+				}
+			}				
+			solo.clickOnButton("Log in using Tequila");
+		}
 	}
 }
