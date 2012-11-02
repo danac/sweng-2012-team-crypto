7763afe7832d3b28853758361db74ea2b7838802
Dana Christen <dana.christen@epfl.ch>
2012-11-02 14:51:15 +0100

    Fixed tests and added new package

---
+++ Sweng2012QuizAppTest/src/epfl/sweng/test/tools/TestingTricks.java
@@ -0,0 +1,28 @@
+package epfl.sweng.test.tools;
+
+import com.jayway.android.robotium.solo.Solo;
+
+import android.app.Activity;
+import android.widget.EditText;
+
+/**
+ * Various GUI manipulation tools...
+ */
+public class TestingTricks {
+	private final static String TEST_USERNAME = "valid";
+	private final static String TEST_PASSWORD = "tutu";
+	
+	public static void authenticateMe(Activity activity, Solo solo) {
+
+		for (EditText et: solo.getCurrentEditTexts()) {
+			if (et.getTag().toString() 
+				== activity.getResources().getText(epfl.sweng.R.string.auth_login_hint)) {
+				solo.enterText(et, TEST_USERNAME);
+			} else if (et.getTag().toString()
+				== activity.getResources().getText(epfl.sweng.R.string.auth_pass_hint)) {
+				solo.enterText(et, TEST_PASSWORD);
+			}
+		}				
+		solo.clickOnButton("Log in using Tequila");
+	}
+}
