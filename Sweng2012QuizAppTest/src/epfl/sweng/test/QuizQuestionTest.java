0e84f16e58fabbab973bb347454500a7adba8065
Cyril Misev <cyril.misev@epfl.ch>
2012-11-16 16:52:52 +0100

    Fix checkstyle

--- Sweng2012QuizAppTest/src/epfl/sweng/test/QuizQuestionTest.java
+++ Sweng2012QuizAppTest/src/epfl/sweng/test/QuizQuestionTest.java
@@ -27,44 +27,44 @@ public class QuizQuestionTest extends TestCase {
             + "id : '123'"
             + "}";
 
-        public void testQuestionOK() throws JSONException {
-            String json = VALID_QUESTION_JSON;
-            QuizQuestion result = new QuizQuestion(json);
-            assertNotNull(result);
-            
-            String q = "The Question...";
-            List<String> answers = new ArrayList<String>();
-            answers.add("Answer 0");
-            Set<String> tags = new HashSet<String>();
-            tags.add("Tag");
-            final int id=13;
-            final int rightAnswer=3;
-            String owner = "Anonymous";
-            QuizQuestion result2 = new QuizQuestion(q, answers, rightAnswer, tags, id, owner);
-            
-            result2.addAnswerAtIndex("Answer 1", rightAnswer-2);
-            result2.addAnswerAtIndex("Answer 2", rightAnswer-1);
-            result2.addAnswerAtIndex("Answer 3", rightAnswer);
-            result2.addAnswerAtIndex("Answer 4", rightAnswer+1);
-            result2.addAnswerAtIndex("Answer 5", rightAnswer+2);
-            
-            String newAnswer = "Modififed Answer";
-            result2.addAnswerAtIndex(newAnswer, 2);
-            assertEquals(newAnswer, result2.getAnswers()[2]);
-            
-            assertEquals(owner, result2.getOwner());
-            result2.removeAnswerAtIndex(rightAnswer);
-            assertEquals(result2.getSolutionIndex(), -1);
-            result2.setSolutionIndex(2);
-            result2.removeAnswerAtIndex(answers.size()-1);
-            result2.removeAnswerAtIndex(0);
-            assertEquals(result2.getSolutionIndex(), 1);
-            
-            int currentSize = result2.getAnswers().length;
-            final int nbrOfNewAnswers = 3;
-            int newLastIndex = currentSize+nbrOfNewAnswers;
-            result2.addAnswerAtIndex(newAnswer, currentSize+2);
-            assertEquals(newLastIndex, result2.getAnswers().length);
+    public void testQuestionOK() throws JSONException {
+        String json = VALID_QUESTION_JSON;
+        QuizQuestion result = new QuizQuestion(json);
+        assertNotNull(result);
+        
+        String q = "The Question...";
+        List<String> answers = new ArrayList<String>();
+        answers.add("Answer 0");
+        Set<String> tags = new HashSet<String>();
+        tags.add("Tag");
+        final int id=13;
+        final int rightAnswer=3;
+        String owner = "Anonymous";
+        QuizQuestion result2 = new QuizQuestion(q, answers, rightAnswer, tags, id, owner);
+        
+        result2.addAnswerAtIndex("Answer 1", rightAnswer-2);
+        result2.addAnswerAtIndex("Answer 2", rightAnswer-1);
+        result2.addAnswerAtIndex("Answer 3", rightAnswer);
+        result2.addAnswerAtIndex("Answer 4", rightAnswer+1);
+        result2.addAnswerAtIndex("Answer 5", rightAnswer+2);
+        
+        String newAnswer = "Modififed Answer";
+        result2.addAnswerAtIndex(newAnswer, 2);
+        assertEquals(newAnswer, result2.getAnswers()[2]);
+        
+        assertEquals(owner, result2.getOwner());
+        result2.removeAnswerAtIndex(rightAnswer);
+        assertEquals(result2.getSolutionIndex(), -1);
+        result2.setSolutionIndex(2);
+        result2.removeAnswerAtIndex(answers.size()-1);
+        result2.removeAnswerAtIndex(0);
+        assertEquals(result2.getSolutionIndex(), 1);
+        
+        int currentSize = result2.getAnswers().length;
+        final int nbrOfNewAnswers = 3;
+        int newLastIndex = currentSize+nbrOfNewAnswers;
+        result2.addAnswerAtIndex(newAnswer, currentSize+2);
+        assertEquals(newLastIndex, result2.getAnswers().length);
             
         
         SwengHttpClientFactory.setInstance(null);
