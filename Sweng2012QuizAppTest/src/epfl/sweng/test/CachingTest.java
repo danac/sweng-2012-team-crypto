23bd980aed02cf817594617c9cf3ad7d5be3f4e4
Dana Christen <dana.christen@epfl.ch>
2012-12-10 23:56:11 +0100

    Extended CachingTest to include ratings

--- Sweng2012QuizAppTest/src/epfl/sweng/test/CachingTest.java
+++ Sweng2012QuizAppTest/src/epfl/sweng/test/CachingTest.java
@@ -126,8 +126,11 @@ public class CachingTest extends ActivityInstrumentationTestCase2<MainActivity> 
 		assertTrue("No items in list view!", l.getChildCount()>0);
 		
 		assertFalse(solo.searchText("error"));
+		
+		solo.clickOnText("Dislike (");
+		assertTrue(solo.searchText("You dislike the question"));
 	}
-	
+		
 	public void testEditAndShowQuestionInCacheAndGoBackOnline() {
 		TestingTricks.authenticateMe(solo);
 		CheckBox chkBox = (CheckBox) solo.getView(epfl.sweng.R.id.main_checkbox_offline);
